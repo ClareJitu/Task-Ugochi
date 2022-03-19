2a8c42780a573170fe2291ad11f1526e4297edbc
Ezejitu Ugochi <claraezejitu@gmail.com>
2022-03-19 19:35:34 +0100

    assignment

--- RueToken.sol
+++ RueToken.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RueToken is ERC20, Ownable {
     uint256 public rate = 1000;
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     constructor() ERC20("RueToken", "RTK") {
         _mint(msg.sender, 1000000);
     }
@@ -17,8 +21,13 @@ contract RueToken is ERC20, Ownable {
     function buyToken(address receiver) public payable{
         buyToken(receiver);
     }
+<<<<<<< Updated upstream
     function balanceOf(address receiver) public view virtual override returns (uint256){   
         return balanceOf(receiver);
+=======
+    function balanceOf(address receiver) public view virtual override returns (uint){   
+         balanceOf(receiver);
+>>>>>>> Stashed changes
 
   }
 }
